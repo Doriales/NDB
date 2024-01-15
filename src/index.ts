5d9e3153fcf48090c29ae3cd78584824595950ee
Alex Stoliarenko <alexstoliarenko.work@gmail.com>
2024-01-15 23:58:11 +0300

    fix: added db URI

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = ''; // DB URI
+const MONGO_URL = 'mongodb+srv://dekolampard:eerPGDvMY6tWnExu@cluster0.zvrownp.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
